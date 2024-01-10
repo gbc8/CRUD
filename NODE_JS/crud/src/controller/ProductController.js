689964baec3647c46d58e635811ca7c43717c3b9
gbc8 <gbc7@cin.ufpe.br>
2024-01-09 23:12:28 -0300

    fix: NODE_JS endpoints adjustment

--- NODE_JS/crud/src/controller/ProductController.js
+++ NODE_JS/crud/src/controller/ProductController.js
@@ -3,25 +3,25 @@ import ProductService from "../service/ProductService.js";
 
 const router = new Router();
 
-router.get('/product', async (req, res) => {
+router.get('api/product', async (req, res) => {
     return res.status(200).json(await ProductService.findAll());
 });
 
-router.get('/product/:id', async (req, res) => {
+router.get('api/product/:id', async (req, res) => {
     const { id } = req.params;
     return res.status(200).json(await ProductService.findById(id));
 });
 
-router.post('/product', async (req, res) => {
+router.post('api/product', async (req, res) => {
     return res.status(200).json(await ProductService.insert(req.body));
 });
 
-router.put('/product/:id', async (req, res) => {
+router.put('api/product/:id', async (req, res) => {
     const { id } = req.params;
     return res.status(200).json(await ProductService.update(req.body, id));
 });
 
-router.delete('/product/:id', async (req, res) => {
+router.delete('api/product/:id', async (req, res) => {
     const { id } = req.params;
     return res.status(200).json(await ProductService.delete(id));
 });
